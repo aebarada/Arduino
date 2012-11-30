8154fec37d8a02820afd284723cb23ad4030faef
Ben <fake@fake.com>
2012-11-30 14:53:46 -0500

    update for 4 sensors in box

--- Ping Sensors/sensors/sensors.ino
+++ Ping Sensors/sensors/sensors.ino
@@ -1,32 +1,54 @@
 unsigned long echo = 0;
-int ultraSoundSignal = 34; // Ultrasound signal pin
+int frontSensor = 34;
+int rightSensor = 37;
+int leftSensor = 36;
+int backSensor = 35;
 unsigned long ultrasoundValue = 0;
  
 void setup()
 {
    Serial.begin(9600);
-   pinMode(ultraSoundSignal,OUTPUT);
+   pinMode(frontSensor,OUTPUT);
+   pinMode(rightSensor,OUTPUT);
+   pinMode(leftSensor,OUTPUT);
+   pinMode(backSensor,OUTPUT);
 }
  
-unsigned long ping(){
-  pinMode(ultraSoundSignal, OUTPUT); // Switch signalpin to output
-  digitalWrite(ultraSoundSignal, LOW); // Send low pulse
+unsigned long ping(int Pin){
+  ultrasoundValue = 0;
+  pinMode(Pin, OUTPUT); // Switch signalpin to output
+  digitalWrite(Pin, LOW); // Send low pulse
   delayMicroseconds(2); // Wait for 2 microseconds
-  digitalWrite(ultraSoundSignal, HIGH); // Send high pulse
+  digitalWrite(Pin, HIGH); // Send high pulse
   delayMicroseconds(5); // Wait for 5 microseconds
-  digitalWrite(ultraSoundSignal, LOW); // Holdoff
-  pinMode(ultraSoundSignal, INPUT); // Switch signalpin to input
-  digitalWrite(ultraSoundSignal, HIGH); // Turn on pullup resistor
-  echo = pulseIn(ultraSoundSignal, HIGH); //Listen for echo
+  digitalWrite(Pin, LOW); // Holdoff
+  pinMode(Pin, INPUT); // Switch signalpin to input
+  digitalWrite(Pin, HIGH); // Turn on pullup resistor
+  echo = pulseIn(Pin, HIGH); //Listen for echo
   ultrasoundValue = (echo / 58.138) * .39; //convert to CM then to inches
   return ultrasoundValue;
 }
- 
+
+void printOutput(long front, long left, long right, long back)
+{ 
+  String output = "\nF:";
+  output += front;
+  output += "\tL:";
+  output += left;
+  output += "\tR:";
+  output += right;
+  output += "\tB:";
+  output += back;
+  Serial.println(output);
+}
  
 void loop()
 {
-   int x = 0;
-   x = ping();
-   Serial.println(x);
+   long front = 0, left=0, right=0, back=0;
+   front = ping(frontSensor);
+   left = ping(leftSensor);
+   right = ping(rightSensor);
+   back = ping(backSensor);
+   printOutput(front, left, right, back);
    delay(250); //delay 1/4 seconds.
 }
